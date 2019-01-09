169418bc74e912198aaf1328a9a79469c8e777e2
Thornwood <gaborkako@gmail.com>
2019-01-09 15:49:43 +0100

    fastforward

--- voidCheck/test.py
+++ voidCheck/test.py
@@ -95,5 +95,83 @@ plt.title('Histogram for gray scale picture')
 plt.show()
 '''
 
+'''
+# Turn image into grayscale
+def toGray(img):
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    return img_gray
+
+
+# Turn image into grayscale
+def toBinary(img):
+    img_binary = cv.threshold(img, 190, 255, cv.THRESH_BINARY)[1]
+    return img_binary
+
+
+# Turn image into grayscale
+def toInvBinary(img):
+    img_invbinary = cv.threshold(img, 190, 255, cv.THRESH_BINARY_INV)[1]
+    return img_invbinary
+
+
+# Search nutzens
+def searchNutzens(img):
+    # Find and draw contours around rectangles
+    contours = cv.findContours(toInvBinary(toGray(img)), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)[1]
+
+    area_list = []
+    for i in contours:
+        area = cv.contourArea(i)
+        area = round(area)
+        area_list.append(area)
+
+    maxarea = max(area_list)
+    good_list = []
+    for index, area in enumerate(area_list):
+        if area > maxarea * .8:
+            good_list.append(index)
+
+    cons = [i for n, i in enumerate(contours) if n in good_list]
+    #cv.drawContours(img, cons, -1, (255, 0, 0), 1)
+    mask = np.ones(img.shape[:2], dtype="uint8") * 255
+    cv.drawContours(mask, cons, -1, 0, -1)
+    mask = toInvBinary(mask)
+    masked_img = cv.bitwise_and(img, img, mask=mask)
+
+    nutzens = cv.equalizeHist(toGray(masked_img))
+
+    return nutzens
+
+
+img2 = searchNutzens(img)
+img2 = cv.threshold(img2, 122, 255, cv.THRESH_BINARY)[1]
+
+contours2 = cv.findContours(img2, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)[1]
+cv.drawContours(img2, contours2, 11, (255, 0, 0), 1)
+'''
+
+
+'''
+# Set up the detector with default parameters.
+detector = cv.SimpleBlobDetector()
+# Create a detector with the parameters
+ver = (cv.__version__).split('.')
+if int(ver[0]) < 3:
+    detector = cv.SimpleBlobDetector()
+else:
+    detector = cv.SimpleBlobDetector_create()
+
+# Detect blobs.
+keypoints = detector.detect(img)
+
+# Draw detected blobs as red circles.
+# cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+im_with_keypoints = cv.drawKeypoints(img, keypoints, np.array([]), (0, 0, 255),
+                                      cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+# Show keypoints
+cv.imshow("Keypoints", im_with_keypoints)
+'''
+
 cv.waitKey(0)
 cv.destroyAllWindows()
--- voidCheck/voidCheck.py
+++ voidCheck/voidCheck.py
@@ -6,66 +6,69 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Read and show image
-img = cv.imread('roentgen/test.gif')
+img = cv.imread('roentgen/test.jpg')
+rows, cols, dims = img.shape
 # (h, w, d) = img.shape
 # print("width={}, height={}, depth={}".format(w, h, d))
-
+# img = cv.resize(img, (0, 0), fx=0.5, fy=0.5)
 
 # Turn image into grayscale
 def toGray(img):
     img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     return img_gray
 
+# Turn image into Blurred
+def toBlur(img):
+    img_blur = cv.GaussianBlur(img, (3, 3), 0)
+    return img_blur
 
-# Turn image into grayscale
-def toBinary(img):
-    img_binary = cv.threshold(img, 190, 255, cv.THRESH_BINARY)[1]
-    return img_binary
+def toMBlur(img):
+    median = cv.medianBlur(img, 5)
+    return median
 
+blur = cv.GaussianBlur(img, (3, 3), 0)
+median = cv.medianBlur(img, 5)
+# bilateral = cv.bilateralFilter(img, 9, 75, 75)
+# imageIntegral = cv.integral(img)
+hist = cv.calcHist([img], [0], None, [256], [0, 255])
+plt.plot(hist)
 
-# Turn image into grayscale
-def toInvBinary(img):
-    img_invbinary = cv.threshold(img, 190, 255, cv.THRESH_BINARY_INV)[1]
-    return img_invbinary
+min_tresh = 35
+max_tresh = 150
 
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img_binary = cv.threshold(img_gray, min_tresh, max_tresh, cv.THRESH_BINARY)[1]
+img_invbinary = cv.threshold(img_gray, min_tresh, max_tresh, cv.THRESH_BINARY_INV)[1]
+otsu = cv.threshold(img_gray, min_tresh, max_tresh, cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
+otsuinv = cv.threshold(img_gray, min_tresh, max_tresh, cv.THRESH_BINARY_INV+cv.THRESH_OTSU)[1]
 
-# Search nutzens
-def searchNutzens(img):
-    # Find and draw contours around rectangles
-    contours = cv.findContours(toInvBinary(toGray(img)), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)[1]
-
-    area_list = []
-    for i in contours:
-        area = cv.contourArea(i)
-        area = round(area)
-        area_list.append(area)
-
-    maxarea = max(area_list)
-    good_list = []
-    for index, area in enumerate(area_list):
-        if area > maxarea * .8:
-            good_list.append(index)
-
-    cons = [i for n, i in enumerate(contours) if n in good_list]
-    #cv.drawContours(img, cons, -1, (255, 0, 0), 1)
-    mask = np.ones(img.shape[:2], dtype="uint8") * 255
-    cv.drawContours(mask, cons, -1, 0, -1)
-    mask = toInvBinary(mask)
-    masked_img = cv.bitwise_and(img, img, mask=mask)
-
-    nutzens = cv.equalizeHist(toGray(masked_img))
-
-    return nutzens
+_, contours, hierarchy = cv.findContours(img_invbinary, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+hierarchy = hierarchy[0]  # get the actual inner list of hierarchy descriptions
 
 
-img2 = searchNutzens(img)
-img2 = cv.threshold(img2, 122, 255, cv.THRESH_BINARY)[1]
 
-contours2 = cv.findContours(img2, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)[1]
-cv.drawContours(img2, contours2, 11, (255, 0, 0), 1)
+cv.drawContours(img, contours, 0, (255, 0, 0), 1)
 
-# cv.imshow('Original image', img)
-cv.imshow('Processed image', img)
+# For each contour, find the bounding rectangle and draw it
+for component in zip(contours, hierarchy):
+    currentContour = component[0]
+    currentHierarchy = component[1]
+    x, y, w, h = cv.boundingRect(currentContour)
+    if currentHierarchy[3] < 0:
+        cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 255), 1)  # these are the innermost child components
+#   elif currentHierarchy[2] < 0:
+#       cv.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 1)  # these are the outermost parent components
+
+cv.imshow('Original image', img)
+# cv.imshow('Gray', img_gray)
+# cv.imshow('Binary', img_binary)
+# cv.imshow('Invert Binary', img_invbinary)
+# cv.imshow('Otsu', otsu)
+# cv.imshow('Invert Otsu', otsuinv)
+# cv.imshow('Gaussian Blurred', blur)
+# cv.imshow('Median Blurred', median)
+# cv.imshow('Bilateral Blurred', bilateral)
+# plt.show()
 
 cv.waitKey(0)
 cv.destroyAllWindows()

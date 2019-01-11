0baae5cc9df34c7fed703b19fd15f078139bc5e4
Thornwood <gaborkako@gmail.com>
2019-01-11 16:06:42 +0100

    Little 5S in code

--- voidCheck/voidCheck.py
+++ voidCheck/voidCheck.py
@@ -5,6 +5,55 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
+# region Image convert functions
+# Convert image to Gray
+def to_gray(image):
+    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    return image
+
+
+# Convert image to Binary
+def to_binary(image, min_tresh, max_tresh):
+    image = cv.threshold(image, min_tresh, max_tresh, cv.THRESH_BINARY)[1]
+    return image
+
+
+# Convert image to Invert Binary
+def to_invbinary(image, min_tresh, max_tresh):
+    image = cv.threshold(image, min_tresh, max_tresh, cv.THRESH_BINARY_INV)[1]
+    return image
+
+
+# Convert image to Otsu
+def to_otsu(image, min_tresh, max_tresh):
+    image = cv.threshold(image, min_tresh, max_tresh, cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
+    return image
+
+
+# Convert image to Invert Otsu
+def to_invotsu(image, min_tresh, max_tresh):
+    image = cv.threshold(image, min_tresh, max_tresh, cv.THRESH_BINARY_INV+cv.THRESH_OTSU)[1]
+    return image
+
+
+# Convert image to Blurred
+def to_blur(image):
+    image = cv.GaussianBlur(image, (3, 3), 0)
+    return image
+
+
+# Convert image to Median Blurred
+def to_mblur(image):
+    image = cv.medianBlur(image, 5)
+    return image
+
+# Calculate Image Histogram
+def calchist(image):
+    image = cv.calcHist([image], [0], None, [256], [0, 255])
+    return image
+# endregion
+
+
 # Read and show image
 img = cv.imread('roentgen/test.jpg')
 rows, cols, dims = img.shape
@@ -12,40 +61,14 @@ rows, cols, dims = img.shape
 # print("width={}, height={}, depth={}".format(w, h, d))
 # img = cv.resize(img, (0, 0), fx=0.5, fy=0.5)
 
-# Turn image into grayscale
-def toGray(img):
-    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    return img_gray
-
-# Turn image into Blurred
-def toBlur(img):
-    img_blur = cv.GaussianBlur(img, (3, 3), 0)
-    return img_blur
-
-def toMBlur(img):
-    median = cv.medianBlur(img, 5)
-    return median
-
-blur = cv.GaussianBlur(img, (3, 3), 0)
-median = cv.medianBlur(img, 5)
-# bilateral = cv.bilateralFilter(img, 9, 75, 75)
-# imageIntegral = cv.integral(img)
-hist = cv.calcHist([img], [0], None, [256], [0, 255])
-plt.plot(hist)
-
+blur = 5
 min_tresh = 35
 max_tresh = 150
 
-img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-img_binary = cv.threshold(img_gray, min_tresh, max_tresh, cv.THRESH_BINARY)[1]
-img_invbinary = cv.threshold(img_gray, min_tresh, max_tresh, cv.THRESH_BINARY_INV)[1]
-otsu = cv.threshold(img_gray, min_tresh, max_tresh, cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
-otsuinv = cv.threshold(img_gray, min_tresh, max_tresh, cv.THRESH_BINARY_INV+cv.THRESH_OTSU)[1]
+plt.plot(to_gray(calchist(img)))
 
-_, contours, hierarchy = cv.findContours(img_invbinary, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+_, contours, hierarchy = cv.findContours(to_invbinary(img, min_tresh, max_tresh), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 hierarchy = hierarchy[0]  # get the actual inner list of hierarchy descriptions
-
-
 
 cv.drawContours(img, contours, 0, (255, 0, 0), 1)
 
